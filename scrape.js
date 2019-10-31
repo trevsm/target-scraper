009881393b9f448b57a32082202eb5aa4dbf3786
unknown <16tsmith55@gmail.com>
2019-10-30 22:50:49 -0600

    working scrape

--- scrape.js
+++ scrape.js
@@ -1,15 +1,24 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-request("https://www.target.com/p/A-14802181", (error, response, html) => {
-  if (!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-    const price = $("[data-test='product-price']");
-    const specs = $("#specAndDescript");
-    // const TCIN = 1-1-6
-    // const UPC = 1-1-7
-    // const DCPI = 1-1-8
-    // parseFloat(price.substr(1))
-    console.log(specs.text());
+const itemList = ["14802181", "12856531", "78305097"];
+
+request(
+  `https://www.target.com/p/A-${itemList[1]}`,
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      const data = ["product-price", "product-savings"];
+
+      const specsRaw = $("#specAndDescript b");
+      let specs = [];
+
+      // parseFloat(price.substr(1))
+
+      specsRaw.each(function(i, e) {
+        specs[i] = $(this).parent();
+        console.log(specs[i].text());
+      });
+    }
   }
-});
+);
